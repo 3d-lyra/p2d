91469cdc0bd1bd1ef9e1481f69a1ebfcd9c82aac
Aleph Nought <aleph.zeroset@gmail.com>
2022-09-29 15:51:12 +0500

    Refactor types

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,4 @@
-export type ModelIn = { [name: string]: { [name: string]: null } };
-export type ModelOut = { [name: string]: { [name: string]: string } };
+export type HozirontalObjectIn = { [name: string]: null };
+export type HozirontalObjectOut = { [name: string]: string };
+export type ModelIn = { [name: string]: HozirontalObjectIn };
+export type ModelOut = { [name: string]: HozirontalObjectOut };
