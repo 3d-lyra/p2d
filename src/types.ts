59d9265f8ebe28c7d94e3dac3083a937324df361
Aleph Nought <aleph.zeroset@gmail.com>
2022-09-29 15:56:32 +0500

    Fix misspelling

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export type HozirontalObjectIn = { [name: string]: null };
-export type HozirontalObjectOut = { [name: string]: string };
-export type ModelIn = { [name: string]: HozirontalObjectIn };
-export type ModelOut = { [name: string]: HozirontalObjectOut };
+export type HorizontalObjectIn = { [name: string]: null };
+export type HorizontalObjectOut = { [name: string]: string };
+export type ModelIn = { [name: string]: HorizontalObjectIn };
+export type ModelOut = { [name: string]: HorizontalObjectOut };
